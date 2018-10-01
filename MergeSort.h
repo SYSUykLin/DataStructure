07e514e5f2a696acd0743dcf561fbda9c06ccd3e
GreenArrow2017 <1522773921@qq.com>
2018-10-01 16:51:41 +0800

    update

--- MergeSort.h
+++ MergeSort.h
@@ -5,4 +5,62 @@
 #ifndef DATA_STRUCTURE_MERGESORT_H
 #define DATA_STRUCTURE_MERGESORT_H
 
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+void __merge(T arr[], int l, int mid, int r) {
+    T *temp = new T[r - l + 1];
+    for (int i = l; i <= r; i++) {
+        temp[i - l] = arr[i];
+    }
+    int i = l, j = mid + 1;
+    for (int k = l; k <= r; k++) {
+        if (i > mid) {
+            arr[k] = temp[j - l];
+            j++;
+        } else if (j > r) {
+            arr[k] = temp[i - l];
+            i++;
+        } else if (temp[i - l] < temp[j - l]) {
+            arr[k] = temp[i - l];
+            i++;
+        } else {
+            arr[k] = temp[j - l];
+            j++;
+        }
+    }
+    delete[] temp;
+}
+
+template<typename T>
+void __mergeSort(T arr, int l, int r) {
+    if (l >= r) {
+        return;
+    } else {
+        int mid = (l + r) / 2;
+        __mergeSort(arr, l, mid);
+        __mergeSort(arr, mid + 1, r);
+        if (arr[mid] > arr[mid + 1]) {
+            __merge(arr, l, mid, r);
+        }
+    }
+}
+
+template<typename T>
+void MergeSort(T arr[], int n) {
+    __mergeSort(arr, 0, n - 1);
+}
+
+template<typename T>
+void upMergeSort(T arr[], int n){
+    for (int sz = 1; sz <= n; sz = sz + sz){
+        for (int i = 0; i + sz < n; i += sz + sz){
+            __merge(arr, i, i+sz-1, min(i+sz+sz-1, n-1));
+        }
+    }
+}
+
 #endif //DATA_STRUCTURE_MERGESORT_H
--- main.cpp
+++ main.cpp
@@ -2,18 +2,24 @@
 #include "SortTestHelper.h"
 #include "SelectionSort.h"
 #include "InsertionSort.h"
+#include "MergeSort.h"
 #include <cstring>
 using namespace std;
 
 int main() {
-    int n = 10000, rangL = 0, rangR = 10000;
+    int n = 100000, rangL = 0, rangR = 10000;
     int *arr = SortTestHelper::generateRandomArray(n, rangL, rangR);
     int *arr1 = SortTestHelper::CopyArray(arr, n);
     int *arr2 = SortTestHelper::CopyArray(arr, n);
+    int *arr3 = SortTestHelper::CopyArray(arr, n);
+    int *arr4 = SortTestHelper::CopyArray(arr, n);
     SortTestHelper::testSort("SelectionSort", SelectionSort, arr, n);
     SortTestHelper::testSort("InsertionSort", InsertionSort, arr1, n);
     SortTestHelper::testSort("InsertionSort_version2", InsertionSort_version2, arr2, n);
-
+    SortTestHelper::testSort("MergeSort", MergeSort, arr3, n);
+    SortTestHelper::testSort("upMergeSort", upMergeSort, arr4, n);
+    delete[] arr4;
+    delete[] arr3;
     delete[] arr;
     delete[] arr1;
     delete[] arr2;
