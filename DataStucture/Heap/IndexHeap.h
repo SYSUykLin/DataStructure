3a6f80bb5032bebed3bb68a26922c368bdfd87ef
GreenArrow2017 <1522773921@qq.com>
2018-10-11 19:26:21 +0800

    indexHeap

--- DataStucture/Heap/IndexHeap.h
+++ DataStucture/Heap/IndexHeap.h
@@ -5,6 +5,11 @@
 #ifndef DATA_STRUCTURE_INDEXHEAP_H
 #define DATA_STRUCTURE_INDEXHEAP_H
 
+#include <iostream>
+#include <algorithm>
+#include <ctime>
+#include <cassert>
+
 template<typename item>
 class IndexHeap {
 private:
@@ -12,6 +17,28 @@ private:
     item *data;
     int count;
     int capacity;
+
+    void shiftUp(int k) {
+        while (k > 1 && data[indexes[k / 2]] < data[indexes[k]]) {
+            swap(indexes[k / 2], indexes[k]);
+            k /= 2;
+        }
+    }
+
+    void shitDown(int k) {
+        while (2 * k <= count) {
+            int change = 2 * k;
+            if (change + 1 <= count && data[indexes[change]] < data[indexes[change + 1]]) {
+                change++;
+            }
+            if (data[indexes[change]] <= data[indexes[k]]) {
+                break;
+            }
+            swap(indexes[change], indexes[k]);
+            k = change;
+        }
+    }
+
 public:
     IndexHeap(int capacity) {
         indexes = new int[capacity + 1];
@@ -23,6 +50,43 @@ public:
     ~IndexHeap() {
         delete[] data;
         delete[] indexes;
+    }
+
+    int size() {
+        return count;
+    }
+
+    bool isEmpty() {
+        return count == 0;
+    }
+
+    void insert(int i, item itemNumber) {
+        assert(count + 1 <= capacity);
+        assert(i + 1 >= 1 && i + 1 < capacity);
+        i++;
+        data[i] = itemNumber;
+        indexes[++count] = i;
+        shiftUp(count);
+    }
+
+    item extractMax(){
+        item num = data[indexes[1]];
+        swap(indexes[1], indexes[count]);
+        count -- ;
+        shitDown(1);
+        return num;
+    }
+
+    void change(int i, item itemNumber){
+        i += 1;
+        data[i] = itemNumber;
+        for (int j = 1; j <= count; ++j) {
+            if (indexes[j] == i){
+                shiftUp(j);
+                shitDown(j);
+                return;
+            }
+        }
     }
 };
 
