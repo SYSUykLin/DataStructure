a4e0b2f7d3f9b5e833d263c52fd87034d70a6c8e
GreenArrow2017 <1522773921@qq.com>
2018-10-13 16:56:49 +0800

    update

--- DataStucture/Tree/BinarySearchTree.h
+++ DataStucture/Tree/BinarySearchTree.h
@@ -8,7 +8,7 @@
 #include <algorithm>
 #include <iostream>
 #include <cassert>
-
+using namespace std;
 template<typename Key, typename Value>
 class BST {
 private:
@@ -35,7 +35,7 @@ public:
     }
 
     ~BST() {
-        //TODO
+        destory();
     }
 
     int size() {
@@ -46,35 +46,106 @@ public:
         return count == 0;
     }
 
-    void insertNode(Key key, Value value){
+    void insertNode(Key key, Value value) {
         root = insert(root, key, value);
     }
 
-    Node *search(Key key){
-
+    Value *search(Key key) {
+        return search(root, key);
     }
 
-    bool contain(Key key){
+    bool contain(Key key) {
+        return contain(root, key);
+    }
 
+    void preOreder(){
+        preOrder(root);
+    }
+
+    void inOrder(){
+        inOrder(root);
+    }
+
+    void postOrder(){
+        postOrder(root);
+    }
+
+    void destory(){
+        destory(root);
     }
 
 private:
-    Node *insert(Node *node, Key key, Value value){
-        if (node == NULL){
-            count ++;
+    Node *insert(Node *node, Key key, Value value) {
+        if (node == NULL) {
+            count++;
             return new Node(key, value);
         }
-        if (node->key == key){
+        if (node->key == key) {
             node->value = value;
-        } else if(key < node->key){
+        } else if (key < node->key) {
             node->left = insert(node->left, key, value);
-        } else{
+        } else {
             node->right = insert(node->right, key, value);
         }
         return node;
     }
 
-    Node *search(Node *node, Key key, Value value){
+    Value *search(Node *node, Key key) {
+        if (NULL == node) {
+            return NULL;
+        } else if (node->key == key) {
+            return &(node->value);
+        } else if (key < node->key) {
+            return search(node->left, key);
+        } else {
+            return search(node->right, key);
+        }
+    }
+
+    bool contain(Node *node, Key key) {
+        if (NULL == node) {
+            return false;
+        }
+        if (node->key == key) {
+            return true;
+        } else if (key < node->key) {
+            return contain(node->left, key);
+        } else {
+            return contain(node->right, key);
+        }
+    }
+
+    void preOrder(Node *node){
+        if (node != NULL){
+            cout << node->value;
+            preOrder(node->left);
+            preOrder(node->right);
+        }
+    }
+
+    void inOrder(Node *node){
+        if (node != NULL){
+            inOrder(node->left);
+            cout << node->value;
+            inOrder(node->right);
+        }
+    }
+
+    void postOrder(Node * node){
+        if (node != NULL){
+            postOrder(node->left);
+            postOrder(node->right);
+            cout << node->value;
+        }
+    }
+
+    void destory(Node *node){
+        if (node != NULL){
+            destory(node->left);
+            destory(node->right);
+            delete node;
+            count --;
+        }
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -7,30 +7,39 @@
 #include "SortAlgorithm/QuickSort.h"
 #include <ctime>
 #include "SortAlgorithm/HeapSort.h"
+#include "DataStucture/Tree/BinarySearchTree.h"
 using namespace std;
 
 int main() {
-    int n = 1000000, rangL = 0, rangR = 100000;
-    int *arr = SortTestHelper::generateRandomArray(n, rangL, rangR);
-    int *arr1 = SortTestHelper::CopyArray(arr, n);
-    int *arr2 = SortTestHelper::CopyArray(arr, n);
-    int *arr3 = SortTestHelper::CopyArray(arr, n);
-    int *arr4 = SortTestHelper::CopyArray(arr, n);
-    int *arr5 = SortTestHelper::CopyArray(arr, n);
-    int *arr6 = SortTestHelper::CopyArray(arr, n);
-
-//    SortTestHelper::testSort("SelectionSort", SelectionSort, arr, n);
-//    SortTestHelper::testSort("InsertionSort", InsertionSort, arr1, n);
-//    SortTestHelper::testSort("InsertionSort_version2", InsertionSort_version2, arr2, n);
-    SortTestHelper::testSort("MergeSort", MergeSort, arr3, n);
-    SortTestHelper::testSort("upMergeSort", upMergeSort, arr4, n);
-    SortTestHelper::testSort("QuickSort", QuickSort, arr5, n);
-    SortTestHelper::testSort("HeapSort", HeapSort_version2, arr6, n);
-    delete[] arr6;
-    delete[] arr5;
-    delete[] arr4;
-    delete[] arr3;
-    delete[] arr;
-    delete[] arr1;
-    delete[] arr2;
+//    int n = 1000000, rangL = 0, rangR = 100000;
+//    int *arr = SortTestHelper::generateRandomArray(n, rangL, rangR);
+//    int *arr1 = SortTestHelper::CopyArray(arr, n);
+//    int *arr2 = SortTestHelper::CopyArray(arr, n);
+//    int *arr3 = SortTestHelper::CopyArray(arr, n);
+//    int *arr4 = SortTestHelper::CopyArray(arr, n);
+//    int *arr5 = SortTestHelper::CopyArray(arr, n);
+//    int *arr6 = SortTestHelper::CopyArray(arr, n);
+//
+////    SortTestHelper::testSort("SelectionSort", SelectionSort, arr, n);
+////    SortTestHelper::testSort("InsertionSort", InsertionSort, arr1, n);
+////    SortTestHelper::testSort("InsertionSort_version2", InsertionSort_version2, arr2, n);
+//    SortTestHelper::testSort("MergeSort", MergeSort, arr3, n);
+//    SortTestHelper::testSort("upMergeSort", upMergeSort, arr4, n);
+//    SortTestHelper::testSort("QuickSort", QuickSort, arr5, n);
+//    SortTestHelper::testSort("HeapSort", HeapSort_version2, arr6, n);
+//    delete[] arr6;
+//    delete[] arr5;
+//    delete[] arr4;
+//    delete[] arr3;
+//    delete[] arr;
+//    delete[] arr1;
+//    delete[] arr2;
+/* ------Sort Test------ */
+BST<int, string> tree = BST<int, string>();
+tree.insertNode(89, "one");
+tree.insertNode(23, "two");
+tree.insertNode(37, "three");
+tree.insertNode(24, "four");
+tree.insertNode(58, "five");
+cout << *tree.search(37) << endl;
  }
