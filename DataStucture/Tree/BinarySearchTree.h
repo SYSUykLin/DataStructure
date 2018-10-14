d9484947dc559913da7a46cdb9f2f6d5eabc3446
GreenArrow2017 <1522773921@qq.com>
2018-10-14 15:19:51 +0800

    BFS finished

--- DataStucture/Tree/BinarySearchTree.h
+++ DataStucture/Tree/BinarySearchTree.h
@@ -5,10 +5,13 @@
 #ifndef DATA_STRUCTURE_BINARYSEARCHTREE_H
 #define DATA_STRUCTURE_BINARYSEARCHTREE_H
 
+#include <queue>
 #include <algorithm>
 #include <iostream>
 #include <cassert>
+
 using namespace std;
+
 template<typename Key, typename Value>
 class BST {
 private:
@@ -22,6 +25,13 @@ private:
             this->key = key;
             this->value = value;
             this->left = this->right = NULL;
+        }
+
+        Node(Node *node){
+            this->key = node->key;
+            this->value = node->value;
+            this->left = node->left;
+            this->right = node->right;
         }
     };
 
@@ -58,23 +68,134 @@ public:
         return contain(root, key);
     }
 
-    void preOreder(){
+    void preOreder() {
         preOrder(root);
     }
 
-    void inOrder(){
+    void inOrder() {
         inOrder(root);
     }
 
-    void postOrder(){
+    void postOrder() {
         postOrder(root);
     }
 
-    void destory(){
+    void destory() {
         destory(root);
     }
 
+    void levelOrder() {
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty()) {
+            Node *p = q.front();
+            q.pop();
+            cout << p->value;
+            if (p->left) {
+                q.push(p->left);
+            }
+            if (p->right) {
+                q.push(p->right);
+            }
+        }
+    }
+
+    Key minimum() {
+        assert(count != 0);
+        Node *node = minimum(root);
+        return node->key;
+    }
+
+    Key maximum(){
+        assert(count != 0);
+        Node *node = maximum(root);
+        return node->key;
+    }
+
+    void removeMin(){
+        if (root){
+            root = removeMin(root);
+        }
+    }
+
+    void removeMax(){
+        if (root){
+            root = removeMax(root);
+        }
+    }
+
+    void remove(Key key){
+        root = remove(root, key);
+    }
 private:
+
+    Node *remove(Node *node, Key key){
+        if ( node == NULL){
+            return NULL;
+        } else if (key < node->key){
+            node->left = remove(node->left, key);
+        } else if (key > node->key){
+            node->right = remove(node->right, key);
+        } else{
+            if (node->left == NULL){
+                Node *right = node->right;
+                delete node;
+                count --;
+                return right;
+            } else if (node->right == NULL){
+                Node *left = node->left;
+                delete node;
+                count --;
+                return left;
+            } else{
+                Node *delNode = node;
+                Node *successor = new Node(minimum(node->right));
+                count ++;
+                successor->right = removeMin(node->right);
+                successor->left = node->left;
+                delete delNode;
+                count --;
+                return successor;
+            }
+        }
+    }
+
+    Node *removeMax(Node *node){
+        if (node->right == NULL){
+            Node *left = node->left;
+            delete node;
+            count --;
+            return left;
+        }
+        node->right = removeMax(node->right);
+        return node;
+    }
+
+    Node *removeMin(Node *node){
+        if (node->left == NULL){
+            Node *right = node->right;
+            delete node;
+            count --;
+            return right;
+        }
+        node->left = removeMin(node->left);
+        return node;
+    }
+
+    Node *minimum(Node *node) {
+        if (node->left == NULL) {
+            return node;
+        }
+        return minimum(node->left);
+    }
+
+    Node *maximum(Node *node) {
+        if (node->right){
+            return node;
+        }
+        return maximum(node->right);
+    }
+
     Node *insert(Node *node, Key key, Value value) {
         if (node == NULL) {
             count++;
@@ -115,36 +236,36 @@ private:
         }
     }
 
-    void preOrder(Node *node){
-        if (node != NULL){
+    void preOrder(Node *node) {
+        if (node != NULL) {
             cout << node->value;
             preOrder(node->left);
             preOrder(node->right);
         }
     }
 
-    void inOrder(Node *node){
-        if (node != NULL){
+    void inOrder(Node *node) {
+        if (node != NULL) {
             inOrder(node->left);
             cout << node->value;
             inOrder(node->right);
         }
     }
 
-    void postOrder(Node * node){
-        if (node != NULL){
+    void postOrder(Node *node) {
+        if (node != NULL) {
             postOrder(node->left);
             postOrder(node->right);
             cout << node->value;
         }
     }
 
-    void destory(Node *node){
-        if (node != NULL){
+    void destory(Node *node) {
+        if (node != NULL) {
             destory(node->left);
             destory(node->right);
             delete node;
-            count --;
+            count--;
         }
     }
 };
