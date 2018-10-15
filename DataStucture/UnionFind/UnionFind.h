6e93a2477948ef5c302104830bbb39f80584925a
GreenArrow2017 <1522773921@qq.com>
2018-10-15 19:48:45 +0800

    unionFind_version1

--- DataStucture/UnionFind/UnionFind.h
+++ DataStucture/UnionFind/UnionFind.h
@@ -9,44 +9,45 @@
 #include <algorithm>
 #include <cassert>
 
-class unionFind {
-private:
-    int *id;
-    int count;
-public:
-    unionFind(int n) {
-        count = n;
-        id = new int(n);
-        for (int i = 0; i < n; ++i) {
-            id[i] = i;
-        }
-    }
-
-    ~unionFind() {
-        delete[] id;
-    }
-
-    int find(int p) {
-        assert(p >= 0 && p <= count);
-        return id[p];
-    }
-
-    bool isConnected(int p, int q) {
-        return find(p) == find(q);
-    }
-
-    void unionElements(int p, int q){
-        int pID = find(p);
-        int qID = find(q);
-        if (pID == qID){
-            return;
-        }
-        for (int i = 0; i < count; ++i) {
-            if (id[i] == pID){
-                id[i] = qID;
+namespace UF_version1 {
+    class unionFind {
+    private:
+        int *id;
+        int count;
+    public:
+        explicit unionFind(int n) {
+            count = n;
+            id = new int(n);
+            for (int i = 0; i < n; ++i) {
+                id[i] = i;
             }
         }
-    }
-};
 
+        ~unionFind() {
+            delete[] id;
+        }
+
+        int find(int p) {
+            assert(p >= 0 && p <= count);
+            return id[p];
+        }
+
+        bool isConnected(int p, int q) {
+            return find(p) == find(q);
+        }
+
+        void unionElements(int p, int q) {
+            int pID = find(p);
+            int qID = find(q);
+            if (pID == qID) {
+                return;
+            }
+            for (int i = 0; i < count; ++i) {
+                if (id[i] == pID) {
+                    id[i] = qID;
+                }
+            }
+        }
+    };
+}
 #endif //DATA_STRUCTURE_UNIONFIND_H
