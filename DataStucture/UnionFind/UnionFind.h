050ae1de4c9b33be74a41dfb71433eb0a83e5dfe
GreenArrow2017 <1522773921@qq.com>
2018-10-17 20:36:48 +0800

    unionFind_version2

--- DataStucture/UnionFind/UnionFind.h
+++ DataStucture/UnionFind/UnionFind.h
@@ -90,10 +90,64 @@ namespace UF_version2 {
             if (pRoot == qRoot) {
                 return;
             }
-            if (sz[pRoot] < sz[pRoot]){
+            if (sz[pRoot] < sz[pRoot]) {
                 parent[pRoot] = qRoot;
                 sz[qRoot] += sz[pRoot];
-            } else{
+            } else {
+                parent[qRoot] = pRoot;
+                sz[pRoot] += sz[qRoot];
+            }
+        }
+    };
+}
+
+namespace UF_version3 {
+    class unionFind {
+    private:
+        int *parent;
+        int *sz;
+        int count;
+    public:
+        explicit unionFind(int n) {
+            parent = new int[n];
+            sz = new int[n];
+            count = n;
+            for (int i = 0; i < n; ++i) {
+                parent[i] = i;
+                sz[i] = 1;
+            }
+        }
+
+        ~unionFind() {
+            delete[] parent;
+        }
+
+        int find(int p) {
+            assert(p >= 0 && p <= count);
+//            while (parent[p] != p) {
+//                parent[p] = parent[parent[p]];
+//                p = parent[p];
+//            }
+            if (p != parent[p]) {
+                parent[p] = find(parent[p]);
+            }
+            return parent[p];
+        }
+
+        bool isConnected(int p, int q) {
+            return find(p) == find(q);
+        }
+
+        void unionElements(int p, int q) {
+            int pRoot = find(p);
+            int qRoot = find(q);
+            if (pRoot == qRoot) {
+                return;
+            }
+            if (sz[pRoot] < sz[pRoot]) {
+                parent[pRoot] = qRoot;
+                sz[qRoot] += sz[pRoot];
+            } else {
                 parent[qRoot] = pRoot;
                 sz[pRoot] += sz[qRoot];
             }
